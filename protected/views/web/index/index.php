d598c78875e3bf181b2f396bd79ef7fb6ec3caa3
koigiang <koigiang@gmail.com>
2016-01-22 01:38:30 +0700

    style resposite

--- protected/views/web/index/index.php
+++ protected/views/web/index/index.php
@@ -15,16 +15,19 @@ Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl."/web/
                     <Hr/>
                     <Hr/>
                 </aside>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                    laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
-                    dolores eos qui ratione voluptatem sequi nesciunt.
-                </p>
+                <div class="bs-callout bs-callout-warning">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                        mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+                        dolores eos qui ratione voluptatem sequi nesciunt.
+                    </p>
+                </div>
+
                 <aside id="wen-corporate-service-3" class="widget wen_corporate_service_widget"><h2
                         class="widget-title"><span>Dịch vụ của chúng tôi</span></h2>
                     <div class="service-block-list row">
--- protected/views/web/layouts/_topmenu.php
+++ protected/views/web/layouts/_topmenu.php
@@ -1,38 +1,65 @@
-<body class="home blog"><div class="mm-menu mm-offcanvas" id="mob-menu"><div id="menu-home" class="mm-panel mm-opened mm-current"><ul class="menu mm-listview mm-first mm-last"><li id="menu-item-7" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-7"><a href="http://mobileplus.vn/">Trang chủ</a></li>
-            <li id="menu-item-17" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-17"><a href="http://mobileplus.vn/sample-page/">Sản phẩm</a></li>
-            <li id="menu-item-16" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16"><a href="http://mobileplus.vn/about/">Giới thiệu</a></li>
-            <li id="menu-item-27" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27"><a href="http://mobileplus.vn/contact-us/">Liên hệ</a></li>
-        </ul></div></div>
-
+<body class="home blog">
 
 <div id="page" class="hfeed site container mm-page mm-slideout">
-    <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
-    <a href="#mob-menu" id="mobile-trigger"><i class="fa fa-th-list"></i></a>
-
-
-
     <header id="masthead" class="site-header" role="banner">
+        <div id="header_phone" class="row" style="display: none">
+            <div class="col-md-12 no_padding">
+                <div class="container">
+                    <div class="col-xs-2 no_padding">
+                        <a href="javascript:;" id="m_sidebar_user">
+                            <div class="icon_menu_phone"></div>
+                        </a>
+                    </div>
+                    <div class="col-xs-8 no_padding area_logo">
+                        <a href="#"><img src="/web/images/logo_wap.png" class="logo_waka_phone"></a>
+                        <div class="site-branding">
+                            <h1 class="site-title">
+                                <a rel="home" href="http://mobileplus.vn/">
+                                    Công ty Khởi Nguồn
+                                </a>
+                            </h1>
 
+                            <h2 class="site-description">Song hành tới thành công</h2>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 no_padding">
+                        <a href="javascript:;" id="m_sidebar_user">
+                            <div class="icon_menu_lang"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--<div id="ab_menu_for_mobile" class="ab_menu_for_mobile" style="display: none">
+                <div class="active" style="width: 50%; float: left">SÁCH</div>
+                <div onclick="showAlert('Tính năng đang được cập nhật. Mời Quý khách tiếp tục sử dụng dịch vụ<div style=\'text-align: center; padding-top:10px;\'><button class=\'btn_custom\' data-dismiss=\'modal\'>OK</button></div>');" style="width: 50%; float: left">AUDIOBOOK</div>
+            </div>-->
+        </div>
         <div class="row header-layout-1">
-            <img class="logo-main" src="<?php echo Yii::app()->request->baseUrl?>/web/images/logo.jpg">
+            <img class="logo-main" src="<?php echo Yii::app()->request->baseUrl ?>/web/images/logo.jpg">
+
             <div class="col-sm-6 site-branding">
                 <h1 class="site-title">
                     <a href="http://mobileplus.vn/" rel="home">
                         Công ty Khởi Nguồn
                     </a>
                 </h1>
+
                 <h2 class="site-description">Song hành tới thành công</h2>
-            </div><!-- .site-branding -->
+            </div>
+            <!-- .site-branding -->
             <div class="col-sm-4 header-area-right">
                 <a href="#" id="btn-search-icon"><i class="fa fa-search"></i></a>
+
                 <div id="header-search-form">
                     <form role="search" method="get" class="search-form" action="http://mobileplus.vn/">
                         <span class="screen-reader-text">Search for:</span>
                         <input class="search-field" placeholder="Search..." name="s" title="Search for:" type="search">
                         <input class="search-submit" value="" type="submit">
                     </form>
-                </div><!-- #header-search-form -->
-                <div class="widget wen_corporate_social_widget"></div>          </div>
+                </div>
+                <!-- #header-search-form -->
+                <div class="widget wen_corporate_social_widget"></div>
+            </div>
         </div>
 
         <div class="" style="display:none;">
@@ -42,14 +69,21 @@
         <div id="site-navigation">
             <div class="main-container">
                 <nav class="main-navigation">
-                    <ul id="menu-home-1" class="menu"><li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-7"><a href="http://mobileplus.vn/">Trang chủ</a></li>
-                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-17"><a href="http://mobileplus.vn/sample-page/">Sản phẩm</a></li>
-                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16"><a href="http://mobileplus.vn/about/">Giới thiệu</a></li>
-                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27"><a href="http://mobileplus.vn/contact-us/">Liên hệ</a></li>
-                    </ul>          </nav><!-- .main-navigation -->
+                    <ul id="menu-home-1" class="menu">
+                        <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-7">
+                            <a href="http://mobileplus.vn/">Trang chủ</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-17"><a
+                                href="http://mobileplus.vn/sample-page/">Sản phẩm</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-16"><a
+                                href="http://mobileplus.vn/about/">Giới thiệu</a></li>
+                        <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-27"><a
+                                href="http://mobileplus.vn/contact-us/">Liên hệ</a></li>
+                    </ul>
+                </nav>
+                <!-- .main-navigation -->
             </div>
         </div>
 
 
-
-    </header><!-- #masthead -->
+    </header>
+    <!-- #masthead -->
